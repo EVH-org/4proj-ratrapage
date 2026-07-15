84c0b0296a6035161fe1d5d3b5d633e76fd4c913
EVH-org <eliot.varigault@gmail.com>
2026-07-15 16:08:54 +0200

    Corrige le down_revision de la migration planning (0007 au lieu de 0007_add_recipe_visibility)

--- server/migrations/versions/0008_add_meal_plan_entries.py
+++ server/migrations/versions/0008_add_meal_plan_entries.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 
 revision: str = "0008"
-down_revision: Union[str, None] = "0007_add_recipe_visibility"
+down_revision: Union[str, None] = "0007"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
